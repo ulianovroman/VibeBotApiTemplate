321b8afcbf7bd3dfc6f360d2edb13879782e55f0
Roman <lntyrd@gmail.com>
2026-03-13 10:05:52 +0200

    Refactor TelegramWebhookController endpoints and cleanup

--- Controllers/TelegramWebhookController.cs
+++ Controllers/TelegramWebhookController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using BotApiTemplate.UpdateChainOfResponsibility;
@@ -10,16 +9,13 @@ namespace BotApiTemplate.Controllers;
 [Route("api/telegram")]
 public class TelegramWebhookController : ControllerBase
 {
-    private readonly ILogger<TelegramWebhookController> _logger;
     private readonly ITelegramBotClient _telegramBotClient;
     private readonly UpdateProcessor _processor;
 
     public TelegramWebhookController(
-        ILogger<TelegramWebhookController> logger,
         ITelegramBotClient telegramBotClient,
         UpdateProcessor processor)
     {
-        _logger = logger;
         _telegramBotClient = telegramBotClient;
         _processor = processor;
     }
@@ -41,23 +37,11 @@ public class TelegramWebhookController : ControllerBase
         return Ok();
     }
 
-    [HttpGet("getWebhookSettings")]
+    [HttpGet("webhook/settings")]
     public async Task<IActionResult> GetWebhookSettings(CancellationToken cancellationToken)
-    {      
+    {
         var result = await _telegramBotClient.GetWebhookInfo(cancellationToken);
 
         return Ok(result);
     }
-
-    //[HttpDelete("deleteWebhookSettings")]
-    //public async Task<IActionResult> DeleteWebhookSettings(CancellationToken cancellationToken)
-    //{
-    //    if (_telegramBotClient is null)
-    //    {
-    //        return Problem("TELEGRAM_BOT_TOKEN is not configured");
-    //    }
-
-    //    await _telegramBotClient.DeleteWebhook(cancellationToken: cancellationToken);
-    //    return Ok();
-    //}
 }
